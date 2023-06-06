2dbe79dcac513746714a29f9532af316ed9fde09
Jason <jason@Jasons-MacBook-Air.local>
2023-06-06 14:58:29 -0700

    right paddle is now moving

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const lP = document.getElementById('leftpaddle');
 const rP = document.getElementById('rightpaddle');
+const puck =document.getElementById('ball');
 let rightTop = 100;
 let leftTop =200;
 
@@ -10,11 +11,11 @@ function leftPaddle(y){
     console.log(leftTop+y+'px');
     leftTop += y;
 }
-function rightPaddle(){
+function rightPaddle(y){
     console.log(rightTop);
     rP.style.top =rightTop + y + 'px';
     console.log(rightTop+y+'px');
-    rightTop -= y;
+    rightTop += y;
 }
 function ball(){
   
@@ -22,23 +23,11 @@ function ball(){
 //key movements
 
 document.addEventListener('keydown', move, false);
-
+document.addEventListener('keydown', moveR, false);
 document.addEventListener('keyup', keyUp, false);
 var key=[];
 
-function move(e){
-    let y = 0;
-    key[e.keyCode]=true;
-    if(key[40]){
-        y+=20;
-    }
-    if(key[38]){
-        y-=20;
-    }
-    e.preventDefault();
-    rightPaddle(y);
-    console.log(y)
-}
+
 function move(e){
     let y = 0;
     key[e.keyCode]=true;
@@ -52,9 +41,27 @@ function move(e){
     leftPaddle(y);
     console.log(y)
 }
+function moveR(e){
+    let y = 0;
+    key[e.keyCode]=true;
+    if(key[40]){
+        y+=20;
+    }
+    if(key[38]){
+        y-=20;
+    }
+    e.preventDefault();
+    rightPaddle(y);
+    console.log(y)
+}
 function keyUp(e){
     key[e.keyCode]=false;
 }
+
+//adding stopping points for paddles
+
+
+
 //restart button
 document.getElementById('reset').addEventListener('mouseover',mouseOver);
 function mouseOver(){
